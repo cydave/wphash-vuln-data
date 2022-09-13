3a8e333b9b5d17333aed8d9364a4ca9e98554e64
dave <dave@cyllective.com>
2022-09-13 18:30:48 +0200

    update

--- utils/sort_wp.py
+++ utils/sort_wp.py
@@ -7,16 +7,18 @@ import nvd
 
 
 if __name__ == "__main__":
-    for nvd_entry_filepath in glob.iglob("../unsorted/CVE-*.json"):
-        with open(nvd_entry_filepath) as fin:
-            nvd_entry = json.load(fin)
-            entries = nvd.extract_cpes(nvd_entry)
-            allwp = True
-            for entry in entries:
-                if not entry.startswith("cpe:2.3:a:wordpress:wordpress:"):
-                    allwp = False
-                    break
-            if allwp:
-                fname = os.path.basename(nvd_entry_filepath)
-                fdest = f"../sorted/wordpress-core/{fname}"
-                shutil.move(nvd_entry_filepath, fdest)
+    print("Unreliable :(")
+    exit(1)
+#    for nvd_entry_filepath in glob.iglob("../unsorted/CVE-*.json"):
+#        with open(nvd_entry_filepath) as fin:
+#            nvd_entry = json.load(fin)
+#            entries = nvd.extract_cpes(nvd_entry)
+#            allwp = True
+#            for entry in entries:
+#                if not entry.startswith("cpe:2.3:a:wordpress:wordpress:"):
+#                    allwp = False
+#                    break
+#            if allwp:
+#                fname = os.path.basename(nvd_entry_filepath)
+#                fdest = f"../sorted/wordpress-core/{fname}"
+#                shutil.move(nvd_entry_filepath, fdest)
